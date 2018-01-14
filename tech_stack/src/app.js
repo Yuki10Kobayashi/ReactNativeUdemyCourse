dfdd864143bcec47fac2ca4a59d9df7a719ae430
Yuki Kobayashi <yuki10.kb@outlook.com>
2018-01-14 19:39:45 +0900

    copy components

--- tech_stack/src/app.js
+++ tech_stack/src/app.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducers from './reducers/index';
+import { Header } from './components/common/Header';
 
 const App = () => {
   return (
-    <Provider store={createStore}>
-      <View />
+    <Provider store={createStore(reducers)}>
+      <View>
+        <Header headerText="Tech Stack" />
+      </View>
     </Provider>
   );
 };
