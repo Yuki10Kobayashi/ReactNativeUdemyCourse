192150c8833b8a00b6e23f522637d8052a6bd9f5
Yuki Kobayashi <yuki10.kb@outlook.com>
2018-01-29 00:59:00 +0900

    add Router

--- manager/src/App.js
+++ manager/src/App.js
@@ -5,7 +5,7 @@ import ReduxThunk from 'redux-thunk';
 import firebase from 'firebase';
 import FirebaseConfig from './config/FirebaseConfig';
 import reducers from './reducers';
-import LoginForm from './components/LoginForm';
+import Router from './Router';
 
 class App extends Component {
   componentWillMount() {
@@ -17,7 +17,7 @@ class App extends Component {
     const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
     return (
       <Provider store={store}>
-        <LoginForm />
+        <Router />
       </Provider>
     );
   }
