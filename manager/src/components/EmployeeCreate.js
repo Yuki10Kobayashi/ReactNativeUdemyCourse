5380ec859800349859d01096777c6b0788c437bd
Yuki Kobayashi <yuki10.kb@outlook.com>
2018-01-30 00:56:51 +0900

    create buttonのactionのガワを作成

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeCreate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
+  onButtonPress() {
+    const { name, phone, shift } = this.props;
+
+    this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
+  }
+
   render() {
     return (
       <Card>
@@ -43,8 +49,8 @@ class EmployeeCreate extends Component {
         </CardSection>
 
         <CardSection>
-          <Button>
-              Save
+          <Button onPress={this.onButtonPress.bind(this)}>
+              Create
           </Button>
         </CardSection>
       </Card>
@@ -64,4 +70,7 @@ const mapStateToProps = (state) => {
   return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
+export default connect(mapStateToProps, {
+  employeeUpdate,
+  employeeCreate
+})(EmployeeCreate);
